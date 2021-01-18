cad721db39e1abcb2dc36526e206fef20e9699cb
zaki.shaikh <zaki.shaikh@vergehealth.com>
2021-01-18 19:37:32 +0530

    CORS added, HTML updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path');
 const dotenv = require('dotenv')
+const cors = require('cors');
 
 const app = express();
 
@@ -8,6 +9,8 @@ dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(cors());
 
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
 app.use(express.static(path.join(__dirname, 'public')));
